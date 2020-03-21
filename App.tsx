f8f09046b5dc9325f934ead9cbffabd75c1b269f
Andres Serrano <andres.srg@gmail.com>
2020-03-21 00:59:15 -0500

    Install ble lib (#1)
    * init
    
    * Add permissions
    
    * List devices
    
    * Buid config
    
    * On push
    
    * Remove github actions
    
    * Fix permissions
    
    * Install ble lib ci (#2)
    
    * Fix ci again
    
    * Bump
    
    * Cleanup
    
    * Turn of

--- App.tsx
+++ App.tsx
@@ -1,10 +1,62 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { BleManager, Device } from 'react-native-ble-plx';
+import { Permissions } from 'react-native-unimodules';
+
+function DevicesList({ manager }: { manager: BleManager }) {
+  return null;
+}
 
 export default function App() {
+  const [manager] = React.useState(() => new BleManager());
+  const [devices, setDevices] = React.useState<{
+    [id: string]: { foundAt: Date; device: Device };
+  }>({});
+
+  React.useEffect(() => {
+    Permissions.askAsync(Permissions.LOCATION).then(({ status }) => {
+      if (status !== 'granted') {
+        alert('BLE no autorizado');
+        return;
+      }
+
+      function scanAndConnect() {
+        manager.startDeviceScan(null, null, (error, device) => {
+          if (error) {
+            // Handle error (scanning will be stopped automatically)
+            alert(error);
+            return;
+          }
+
+          setDevices(state => ({
+            ...state,
+
+            [device.id]: {
+              foundAt: new Date(),
+              device
+            }
+          }));
+        });
+      }
+
+      const subscription = manager.onStateChange(state => {
+        if (state === 'PoweredOn') {
+          scanAndConnect();
+          subscription.remove();
+        }
+      }, true);
+    });
+  }, [manager]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ fontWeight: 'bold' }}>Nearby BLE devices:</Text>
+
+      <View style={{ height: 20 }} />
+
+      {Object.values(devices).map(({ foundAt, device }) => (
+        <Text key={device.id}>{device.name ?? device.id}</Text>
+      ))}
     </View>
   );
 }
