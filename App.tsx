cbe057bb7484a8206ddbab7c1114093ce900e96b
Andres Serrano <andres.srg@gmail.com>
2020-03-21 01:09:14 -0500

    Allow start and stop devices search

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { BleManager, Device } from 'react-native-ble-plx';
 import { Permissions } from 'react-native-unimodules';
 
@@ -9,44 +9,63 @@ function DevicesList({ manager }: { manager: BleManager }) {
 
 export default function App() {
   const [manager] = React.useState(() => new BleManager());
+
+  const [findDevices, setFindDevices] = React.useState(false);
+
   const [devices, setDevices] = React.useState<{
     [id: string]: { foundAt: Date; device: Device };
   }>({});
 
   React.useEffect(() => {
-    Permissions.askAsync(Permissions.LOCATION).then(({ status }) => {
-      if (status !== 'granted') {
-        alert('BLE no autorizado');
-        return;
-      }
+    let deactivators = [];
 
-      function scanAndConnect() {
-        manager.startDeviceScan(null, null, (error, device) => {
-          if (error) {
-            // Handle error (scanning will be stopped automatically)
-            alert(error);
-            return;
-          }
-
-          setDevices(state => ({
-            ...state,
-
-            [device.id]: {
-              foundAt: new Date(),
-              device
-            }
-          }));
-        });
-      }
-
-      const subscription = manager.onStateChange(state => {
-        if (state === 'PoweredOn') {
-          scanAndConnect();
-          subscription.remove();
+    if (findDevices) {
+      Permissions.askAsync(Permissions.LOCATION).then(({ status }) => {
+        if (status !== 'granted') {
+          alert('BLE no autorizado');
+          return;
         }
-      }, true);
-    });
-  }, [manager]);
+
+        function scanAndConnect() {
+          manager.startDeviceScan(null, null, (error, device) => {
+            if (error) {
+              // Handle error (scanning will be stopped automatically)
+              alert(error);
+              return;
+            }
+
+            setDevices(state => ({
+              ...state,
+
+              [device.id]: {
+                foundAt: new Date(),
+                device
+              }
+            }));
+          });
+
+          deactivators.push(() => manager.stopDeviceScan());
+        }
+
+        let stopStateChangeSubscription;
+
+        const subscription = manager.onStateChange(state => {
+          deactivators.push(stopStateChangeSubscription);
+          if (state === 'PoweredOn') {
+            scanAndConnect();
+            stopStateChangeSubscription();
+            deactivators = deactivators.filter(
+              fn => fn !== stopStateChangeSubscription
+            );
+          }
+        }, true);
+
+        stopStateChangeSubscription = () => subscription.remove();
+      });
+    }
+
+    return () => deactivators.forEach(fn => fn());
+  }, [manager, findDevices]);
 
   return (
     <View style={styles.container}>
@@ -54,9 +73,18 @@ export default function App() {
 
       <View style={{ height: 20 }} />
 
-      {Object.values(devices).map(({ foundAt, device }) => (
-        <Text key={device.id}>{device.name ?? device.id}</Text>
-      ))}
+      {findDevices &&
+        Object.values(devices).map(({ foundAt, device }) => (
+          <Text key={device.id}>{device.name ?? device.id}</Text>
+        ))}
+
+      <Button
+        onPress={() => {
+          setDevices({});
+          setFindDevices(state => !state);
+        }}
+        title={findDevices ? 'Parar la busqueda' : 'Buscar dispositivos'}
+      />
     </View>
   );
 }
