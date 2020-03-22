adfa4d20f209fb28b38e8955d8499002e5e055cf
Andres Serrano <andres.srg@gmail.com>
2020-03-21 19:45:06 -0500

    wip

--- App.tsx
+++ App.tsx
@@ -4,10 +4,135 @@ import { BleManager, Device } from 'react-native-ble-plx';
 import { Permissions } from 'react-native-unimodules';
 import codePush from 'react-native-code-push';
 
-import { VictoryBar, VictoryChart, VictoryLine } from 'victory-native';
+import {
+  VictoryBar,
+  VictoryChart,
+  VictoryLine,
+  VictoryTheme
+} from 'victory-native';
 
 const UART_SERVICE_UUID = '0000FFE0-0000-1000-8000-00805F9B34FB';
 const UART_CHARACTERISTIC_UUID = '0000FFE1-0000-1000-8000-00805F9B34FB';
+
+const X_DURATION = 0.5 * 60 * 1000;
+
+function Chart() {
+  const [data, setData] = React.useState<{ x: number; y: number }[]>([]);
+  const [newData, setNewData] = React.useState<{ x: number; y: number }[]>([]);
+  const [startTime] = React.useState(() => Date.now());
+
+  React.useEffect(() => {
+    let stop = false;
+    const fn = () => {
+      if (stop) return;
+
+      Math.random() > 0.1 &&
+        (() => {
+          const point = {
+            // x: (Date.now() / 1000) % (X_DURATION / 1000),
+            x: Date.now(),
+            // y: Math.random() * 5
+            y: 5 * Math.sin(((Date.now() / 1000) % (X_DURATION / 1000)) / 1)
+          };
+
+          setData(state => [...state, point]);
+        })();
+
+      requestAnimationFrame(fn);
+    };
+
+    requestAnimationFrame(fn);
+
+    return () => (stop = true);
+  }, []);
+
+  const [time, setTime] = React.useState(() => Date.now());
+
+  React.useEffect(() => {
+    let stop = false;
+    const fn = () => {
+      if (stop) return;
+
+      setTime(Date.now());
+
+      requestAnimationFrame(fn);
+    };
+
+    requestAnimationFrame(fn);
+
+    return () => (stop = true);
+  }, []);
+
+  const currentTime = ((time - startTime) / 1000) % (X_DURATION / 1000);
+
+  const prevTimeRef = React.useRef(currentTime);
+
+  const [timeCutoff, setTimeCutoff] = React.useState(time);
+  React.useEffect(() => {
+    // console.log('currentTime', currentTime, 'prevTime', prevTimeRef.current);
+    if (currentTime < prevTimeRef.current) {
+      setTimeCutoff(time);
+    }
+  });
+
+  React.useEffect(() => {
+    prevTimeRef.current = currentTime;
+  });
+
+  const calcData = React.useMemo(
+    () =>
+      data
+        .filter(p => timeCutoff < p.x)
+        .map(p => ({
+          ...p,
+          x: ((p.x - startTime) / 1000) % (X_DURATION / 1000)
+        })),
+    [data]
+  );
+
+  return (
+    <VictoryChart
+      // animate={{ duration: 500 }}
+      theme={VictoryTheme.material}
+      domainPadding={20}
+      domain={{
+        x: [0, X_DURATION / 1000],
+        y: [-5, 5]
+        // y: [
+        //   Math.min(0, ...data.map(d => d.y)),
+        //   Math.max(0, ...data.map(d => d.y))
+        // ]
+      }}
+    >
+      {/* <VictoryLine
+        samples={25}
+        y={d => Math.sin(5 * Math.PI * d.x)}
+        interpolation="natural"
+      /> */}
+      <VictoryLine
+        style={{ data: { stroke: 'red' } }}
+        data={[
+          {
+            x: currentTime,
+            y: -100
+          },
+          {
+            x: currentTime,
+            y: 100
+          }
+        ]}
+      />
+
+      <VictoryLine
+        // animate={{ duration: 500 }}
+        // samples={100}
+        // style={{ data: { stroke: 'red' } }}
+        data={calcData}
+        // interpolation="natural"
+      />
+    </VictoryChart>
+  );
+}
 
 function App() {
   const [manager] = React.useState(() => new BleManager());
@@ -93,23 +218,27 @@ function App() {
                 <TouchableOpacity
                   onPress={() => {
                     manager.connectToDevice(device.id).then(
-                      () => {
-                        const subscription = device.monitorCharacteristicForService(
-                          UART_SERVICE_UUID,
-                          UART_CHARACTERISTIC_UUID,
-                          (error, characteristic) => {
-                            console.log('received', characteristic.value);
+                      // () => {
+                      //   const subscription = device.monitorCharacteristicForService(
+                      //     UART_SERVICE_UUID,
+                      //     UART_CHARACTERISTIC_UUID,
+                      //     (error, characteristic) => {
+                      //       console.log('received', characteristic.value);
+                      //     }
+                      //   );
+                      // },
+                      () =>
+                        device.discoverAllServicesAndCharacteristics().then(
+                          async device => {
+                            alert(
+                              'services: ' +
+                                JSON.stringify(device.serviceData, null, 2)
+                            );
+                          },
+                          err => {
+                            alert('failed');
                           }
-                        );
-                      },
-                      // device.discoverAllServicesAndCharacteristics().then(
-                      //   () => {
-                      //     console.log('device');
-                      //   },
-                      //   err => {
-                      //     console.log('failed', err);
-                      //   }
-                      // ),
+                        ),
 
                       err => {
                         alert('Fallo de conexión');
@@ -127,19 +256,7 @@ function App() {
 
       {/* <VictoryBar /> */}
 
-      {/* <VictoryChart>
-        <VictoryLine
-          samples={25}
-          y={d => Math.sin(5 * Math.PI * d.x)}
-          interpolation="natural"
-        />
-        <VictoryLine
-          samples={100}
-          style={{ data: { stroke: 'red' } }}
-          y={d => Math.cos(5 * Math.PI * d.x)}
-          interpolation="natural"
-        />
-      </VictoryChart> */}
+      {!findDevices && <Chart />}
 
       <View style={{ alignItems: 'center' }}>
         <Button
