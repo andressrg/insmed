8107b56ae77bac1c81c72efea63c71b8940a6080
Andres Serrano <andres.srg@gmail.com>
2020-03-25 21:07:08 -0500

    Navigation setup (#13)
    * Install libs
    
    * Setup basic screens
    
    * Enable react native screens

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+import 'react-native-gesture-handler';
+import { enableScreens } from 'react-native-screens';
 import { AppRegistry } from 'react-native';
+
 import App from './src/index';
 import { name as appName } from './app.json';
+
+enableScreens();
 
 AppRegistry.registerComponent(appName, () => App);
