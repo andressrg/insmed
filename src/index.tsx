e78a2ad4f8143670df2dd38ddba35be408908f3c
Andres Serrano <andres.srg@gmail.com>
2020-03-25 21:03:05 -0500

    Setup basic screens

--- src/index.tsx
+++ src/index.tsx
@@ -17,6 +17,9 @@ import {
   LayoutProvider
 } from 'recyclerlistview';
 import { useAsync } from 'react-async';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useNavigation } from '@react-navigation/native';
 
 import { Chart } from './components/Chart';
 import { SQLiteContextProvider, SQLiteContext } from './components/SQLContext';
@@ -87,10 +90,26 @@ function Measurements() {
   );
 }
 
-function App() {
-  const [manager] = React.useState(() => new BleManager());
+function HomeScreen() {
+  return false ? (
+    <Measurements />
+  ) : (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Chart />
+    </SafeAreaView>
+  );
+}
 
-  const [findDevices, setFindDevices] = React.useState(true);
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+function DeviceScanScreen() {
+  const [manager] = React.useState(() => new BleManager());
 
   const [devices, setDevices] = React.useState<{
     [id: string]: { foundAt: Date; device: Device };
@@ -99,159 +118,176 @@ function App() {
   React.useEffect(() => {
     let deactivators: (() => void)[] = [];
 
-    if (findDevices) {
-      Permissions.askAsync(Permissions.LOCATION).then(({ status }) => {
-        if (status !== 'granted') {
-          alert('BLE no autorizado');
-          return;
-        }
+    Permissions.askAsync(Permissions.LOCATION).then(({ status }) => {
+      if (status !== 'granted') {
+        alert('BLE no autorizado');
+        return;
+      }
 
-        function scanAndConnect() {
-          manager.startDeviceScan(null, null, (error, device) => {
-            if (error) {
-              // Handle error (scanning will be stopped automatically)
-              alert(error);
-              return;
-            }
-
-            device != null &&
-              setDevices(state => ({
-                ...state,
-
-                [device.id]: {
-                  foundAt: new Date(),
-                  device
-                }
-              }));
-          });
-
-          deactivators.push(() => manager.stopDeviceScan());
-        }
-
-        let stopStateChangeSubscription;
-
-        const subscription = manager.onStateChange(state => {
-          deactivators.push(stopStateChangeSubscription);
-          if (state === 'PoweredOn') {
-            scanAndConnect();
-            stopStateChangeSubscription();
-            deactivators = deactivators.filter(
-              fn => fn !== stopStateChangeSubscription
-            );
+      function scanAndConnect() {
+        manager.startDeviceScan(null, null, (error, device) => {
+          if (error) {
+            // Handle error (scanning will be stopped automatically)
+            alert(error);
+            return;
           }
-        }, true);
 
-        stopStateChangeSubscription = () => subscription.remove();
-      });
-    }
+          device != null &&
+            setDevices(state => ({
+              ...state,
+
+              [device.id]: {
+                foundAt: new Date(),
+                device
+              }
+            }));
+        });
+
+        deactivators.push(() => manager.stopDeviceScan());
+      }
+
+      let stopStateChangeSubscription;
+
+      const subscription = manager.onStateChange(state => {
+        deactivators.push(stopStateChangeSubscription);
+        if (state === 'PoweredOn') {
+          scanAndConnect();
+          stopStateChangeSubscription();
+          deactivators = deactivators.filter(
+            fn => fn !== stopStateChangeSubscription
+          );
+        }
+      }, true);
+
+      stopStateChangeSubscription = () => subscription.remove();
+    });
 
     return () => deactivators.forEach(fn => fn());
-  }, [manager, findDevices]);
+  }, [manager]);
 
-  return false ? (
-    <Measurements />
-  ) : (
+  return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={{ height: 20 }} />
 
-      <Text style={{ fontWeight: 'bold', textAlign: 'center' }}>
-        Dispositivos cercanos:
-      </Text>
-
-      <View style={{ height: 20 }} />
-
       <ScrollView contentContainerStyle={{ alignItems: 'center' }}>
-        {findDevices &&
-          Object.values(devices)
-            .filter(({ device }) => device.name != null)
-            .sort((a, b) => ('' + a.device.name!).localeCompare(b.device.name!))
-            .map(({ device }) => (
-              <React.Fragment key={device.id}>
-                <TouchableOpacity
-                  onPress={() => {
-                    manager
-                      .connectToDevice(device.id)
-                      .then(
-                        () => {
-                          alert(`Connected to ${device.name}`);
+        {Object.values(devices)
+          .filter(({ device }) => device.name != null)
+          .sort((a, b) => ('' + a.device.name!).localeCompare(b.device.name!))
+          .map(({ device }) => (
+            <React.Fragment key={device.id}>
+              <TouchableOpacity
+                onPress={() => {
+                  manager
+                    .connectToDevice(device.id)
+                    .then(
+                      () => {
+                        alert(`Connected to ${device.name}`);
 
-                          return device
-                            .discoverAllServicesAndCharacteristics()
-                            .then(
-                              async device => {
-                                alert(
-                                  `services for ${device.name}: ${(
-                                    await Promise.all(
-                                      (await device.services()).map(service =>
-                                        service
-                                          .characteristics()
-                                          .then(characteristics => ({
-                                            service,
-                                            characteristics
-                                          }))
-                                      )
+                        return device
+                          .discoverAllServicesAndCharacteristics()
+                          .then(
+                            async device => {
+                              alert(
+                                `services for ${device.name}: ${(
+                                  await Promise.all(
+                                    (await device.services()).map(service =>
+                                      service
+                                        .characteristics()
+                                        .then(characteristics => ({
+                                          service,
+                                          characteristics
+                                        }))
                                     )
                                   )
-                                    .map(
-                                      ({ service, characteristics }) =>
-                                        `service: ${
-                                          service.id
-                                        } ${characteristics
-                                          .map(
-                                            characteristic =>
-                                              characteristic.uuid
-                                          )
-                                          .join(', ')}`
-                                    )
-                                    .join('; ')}`
-                                );
-                              },
-                              err => {
-                                alert('failed');
-                              }
-                            );
-                        },
+                                )
+                                  .map(
+                                    ({ service, characteristics }) =>
+                                      `service: ${
+                                        service.id
+                                      } ${characteristics
+                                        .map(
+                                          characteristic => characteristic.uuid
+                                        )
+                                        .join(', ')}`
+                                  )
+                                  .join('; ')}`
+                              );
+                            },
+                            err => {
+                              alert('failed');
+                            }
+                          );
+                      },
 
-                        err => {
-                          alert('Fallo de conexión');
-                        }
-                      )
-                      .catch(err => {
-                        alert(`Error ${err} ${JSON.stringify(err)}`);
-                      });
-                  }}
-                >
-                  <Text>{device.name}</Text>
-                </TouchableOpacity>
+                      err => {
+                        alert('Fallo de conexión');
+                      }
+                    )
+                    .catch(err => {
+                      alert(`Error ${err} ${JSON.stringify(err)}`);
+                    });
+                }}
+              >
+                <Text>{device.name}</Text>
+              </TouchableOpacity>
 
-                <View style={{ height: 10 }} />
-              </React.Fragment>
-            ))}
+              <View style={{ height: 10 }} />
+            </React.Fragment>
+          ))}
       </ScrollView>
-
-      {!findDevices && <Chart />}
-
-      <View style={{ alignItems: 'center' }}>
-        <Button
-          onPress={() => {
-            setDevices({});
-            setFindDevices(state => !state);
-          }}
-          title={
-            findDevices
-              ? 'Parar la busqueda'
-              : 'Buscar dispositivos disponibles'
-          }
-        />
-      </View>
-      <View style={{ height: 20 }} />
     </SafeAreaView>
+  );
+}
+
+const MainStack = createStackNavigator();
+const RootStack = createStackNavigator();
+
+function MainStackScreen() {
+  const navigation = useNavigation();
+
+  return (
+    <MainStack.Navigator initialRouteName="Home">
+      <MainStack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: 'Dispositivos conectados',
+
+          headerRight: () => (
+            <View style={{ paddingRight: 10 }}>
+              <Button
+                onPress={() => navigation.navigate('DeviceScan')}
+                title="Buscar"
+              />
+            </View>
+          )
+        }}
+      />
+
+      <MainStack.Screen name="Details" component={DetailsScreen} />
+    </MainStack.Navigator>
   );
 }
 
 const AppWithContext = () => (
   <SQLiteContextProvider>
-    <App />
+    <NavigationContainer>
+      <RootStack.Navigator mode="modal">
+        <RootStack.Screen
+          name="Main"
+          component={MainStackScreen}
+          options={{ headerShown: false }}
+        />
+
+        <RootStack.Screen
+          name="DeviceScan"
+          options={{
+            title: 'Dispositivos cercanos'
+          }}
+          component={DeviceScanScreen}
+        />
+      </RootStack.Navigator>
+    </NavigationContainer>
   </SQLiteContextProvider>
 );
 
