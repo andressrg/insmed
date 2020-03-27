e7c85a421d1616cac1f404901b202e3854cc1089
Andres Serrano <andres.srg@gmail.com>
2020-03-27 00:36:04 -0500

    Extract chart

---
+++ src/utils/charts.tsx
@@ -0,0 +1,65 @@
+export async function getLines({
+  wraparoundMillis,
+  firstTsOfForeground,
+  query
+}: {
+  cursor?: string;
+  wraparoundMillis: number;
+  firstTsOfForeground?: number;
+  query: (p: {
+    first: number;
+    cursor?: number;
+  }) => Promise<{
+    edges: { ts: number; y: number }[];
+    cursor?: string;
+  }>;
+}) {
+  const { edges } = await query({
+    first: 500,
+    cursor: firstTsOfForeground && firstTsOfForeground - wraparoundMillis
+  });
+
+  const firstTs = edges[0]?.ts;
+  const lastTs = edges[edges.length - 1]?.ts;
+
+  const hasWraparound = wraparoundMillis < (lastTs ?? 0) - (firstTs ?? 0);
+
+  firstTsOfForeground =
+    firstTsOfForeground != null
+      ? firstTsOfForeground + wraparoundMillis < lastTs
+        ? firstTsOfForeground +
+          wraparoundMillis *
+            Math.floor((lastTs - firstTsOfForeground) / wraparoundMillis)
+        : firstTsOfForeground
+      : hasWraparound
+      ? lastTs
+      : firstTs;
+
+  const offset =
+    (hasWraparound ? firstTsOfForeground : edges[0]?.ts ?? 0) %
+    wraparoundMillis;
+
+  return {
+    background: (hasWraparound
+      ? edges.filter(
+          p =>
+            firstTsOfForeground! - wraparoundMillis <= p.ts &&
+            p.ts < firstTsOfForeground!
+        )
+      : []
+    ).map(p => ({
+      x: (p.ts - offset) % wraparoundMillis,
+      y: p.y
+    })),
+
+    foreground: (hasWraparound
+      ? edges.filter(p => firstTsOfForeground! <= p.ts)
+      : edges
+    ).map(p => ({
+      x: (p.ts - offset) % wraparoundMillis,
+      y: p.y
+    })),
+
+    firstTsOfForeground
+  };
+}
